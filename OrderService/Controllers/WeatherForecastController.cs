b6f6bbd57a611c3ccd707004f5152684a6d19bd8
Enis Gunesen <egunesen@microsoft.com>
2020-09-29 15:07:26 +0300

    Add project files.

---
+++ OrderService/Controllers/WeatherForecastController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dapr.Client;
+using Dapr.Client.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace OrderService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly DaprClient _client;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, DaprClient client)
+        {
+            _logger = logger;
+            _client = client;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<WeatherForecast>> Get()
+        {
+            return await _client.InvokeMethodAsync<IEnumerable<WeatherForecast>>("inventoryservice", "WeatherForecast", new HTTPExtension { Verb = HTTPVerb.Get });
+        }
+    }
+}
